92fa973ecdfa95df62947146cea496a71db672ee
rokey <rokeyyan@qq.com>
2026-06-07 16:40:22 +0800

    fix: remove folder picker permanently (iOS 26 bug), keep file picker only

--- ios/HandoffApp/HandoffApp/Views/FilePickerView.swift
+++ ios/HandoffApp/HandoffApp/Views/FilePickerView.swift
@@ -2,20 +2,13 @@ import SwiftUI
 
 struct FilePickerView: UIViewControllerRepresentable {
     let onFileSelected: (URL) -> Void
-    let pickFolders: Bool
 
-    init(pickFolders: Bool = false, onFileSelected: @escaping (URL) -> Void) {
-        self.pickFolders = pickFolders
+    init(onFileSelected: @escaping (URL) -> Void) {
         self.onFileSelected = onFileSelected
     }
 
     func makeUIViewController(context: Context) -> UIDocumentPickerViewController {
-        let picker: UIDocumentPickerViewController
-        if pickFolders {
-            picker = UIDocumentPickerViewController(documentTypes: ["public.directory"], in: .open)
-        } else {
-            picker = UIDocumentPickerViewController(documentTypes: ["public.item"], in: .import)
-        }
+        let picker = UIDocumentPickerViewController(documentTypes: ["public.item"], in: .import)
         picker.delegate = context.coordinator
         return picker
     }
@@ -23,16 +16,14 @@ struct FilePickerView: UIViewControllerRepresentable {
     func updateUIViewController(_ uiViewController: UIDocumentPickerViewController, context: Context) {}
 
     func makeCoordinator() -> Coordinator {
-        Coordinator(onFileSelected: onFileSelected, pickFolders: pickFolders)
+        Coordinator(onFileSelected: onFileSelected)
     }
 
     class Coordinator: NSObject, UIDocumentPickerDelegate {
         let onFileSelected: (URL) -> Void
-        let pickFolders: Bool
 
-        init(onFileSelected: @escaping (URL) -> Void, pickFolders: Bool) {
+        init(onFileSelected: @escaping (URL) -> Void) {
             self.onFileSelected = onFileSelected
-            self.pickFolders = pickFolders
         }
 
         func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
@@ -44,18 +35,8 @@ struct FilePickerView: UIViewControllerRepresentable {
 
             let secured = url.startAccessingSecurityScopedResource()
             defer { if secured { url.stopAccessingSecurityScopedResource() } }
-
-            if pickFolders {
-                let coordinator = NSFileCoordinator()
-                var coordError: NSError?
-                coordinator.coordinate(readingItemAt: url, options: .withoutChanges, error: &coordError) { readURL in
-                    try? FileManager.default.copyItem(at: readURL, to: tempURL)
-                    onFileSelected(tempURL)
-                }
-            } else {
-                try? FileManager.default.copyItem(at: url, to: tempURL)
-                onFileSelected(tempURL)
-            }
+            try? FileManager.default.copyItem(at: url, to: tempURL)
+            onFileSelected(tempURL)
         }
 
         func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {}
